ebfd2616254cc6d80520123e9e88e36363f7408f
oscarbolzinger <oscar.bolzinger@ecl20.ec-lyon.fr>
2023-05-31 15:35:57 +0200

    plotting dashboard

--- matlab/runEncodedJsonStruct.m
+++ matlab/runEncodedJsonStruct.m
@@ -6,17 +6,100 @@ function result = runEncodedJsonStruct()
 
     % Run battery simulation with function that takes json input
     output = runBatteryJson(jsonstruct);
+    
+
+    % shorthands
+    ne      = 'NegativeElectrode';
+    pe      = 'PositiveElectrode';
+    am      = 'ActiveMaterial';
+    cc      = 'CurrentCollector';
+    elyte   = 'Electrolyte';
+    thermal = 'ThermalModel';
+    ctrl    = 'Control';
+    sd      = 'SolidDiffusion';
 
     states = output.states;
 
-    result_E = cellfun(@(x) x.Control.E, states);
-    result_I = cellfun(@(x) x.Control.I, states);
+    cell_voltage = cellfun(@(x) x.Control.E, states);
+    cell_current = cellfun(@(x) x.Control.I, states);
     time = cellfun(@(x) x.time, states);
-    result_time = time/hour;
+    time_values = time/hour;
 
+    number_of_states = length(states);
+
+    negative_electrode_grid = output.model.(ne).(am).G.cells;
+    electrolyte_grid = output.model.(elyte).G.cells;
+    positive_electrode_grid = output.model.(pe).(am).G.cells;
+
+
+    % Negative Electrode Concentration
+    negative_electrode_concentration = cell(number_of_states, negative_electrode_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(ne).(am).(sd).cSurface ./ 1000;
+
+        for j = 1 : length(new_cell)
+            negative_electrode_concentration{i, j} = new_cell(j);
+        end
+    end
+
+    % Electrolyte Concentration
+    electrolyte_concentration = cell(number_of_states, electrolyte_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(elyte).c ./ 1000;
+
+        for j = 1 : length(new_cell)
+            electrolyte_concentration{i, j} = new_cell(j);
+        end
+    end
+
+    % Positive Electrode Concentration
+    positive_electrode_concentration = cell(number_of_states, positive_electrode_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(pe).(am).(sd).cSurface ./ 1000;
+
+        for j = 1 : length(new_cell)
+            positive_electrode_concentration{i, j} = new_cell(j);
+        end
+    end
+
+    % Negative Electrode Potential
+    negative_electrode_potential = cell(number_of_states, negative_electrode_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(ne).(am).phi;
+
+        for j = 1 : length(new_cell)
+            negative_electrode_potential{i, j} = new_cell(j);
+        end
+    end
+
+    % Electrolyte Potential
+    electrolyte_potential = cell(number_of_states, electrolyte_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(elyte).phi;
+
+        for j = 1 : length(new_cell)
+            electrolyte_potential{i, j} = new_cell(j);
+        end
+    end
+
+    % Positive Electrode Potential
+    positive_electrode_potential = cell(number_of_states, positive_electrode_grid.num);
+
+    for i = 1 : number_of_states
+        new_cell = states{i}.(ne).(am).phi;
+
+        for j = 1 : length(new_cell)
+            positive_electrode_potential{i, j} = new_cell(j);
+        end
+    end
 
 %    plotDashboard(output.model, output.states, 'step', 0);
 
-    result = [result_E, result_I, result_time];
+    result = {number_of_states, cell_voltage, cell_current, time_values, negative_electrode_grid, electrolyte_grid, positive_electrode_grid, negative_electrode_concentration, electrolyte_concentration, positive_electrode_concentration, negative_electrode_potential, electrolyte_potential, positive_electrode_potential};
 
 end
