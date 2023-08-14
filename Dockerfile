d56800db57ea8aedf3e41d5da3062dd77c6ea285
LorenaH84 <135016387+LorenaH84@users.noreply.github.com>
2023-08-14 17:40:56 +0200

    Created docker devcontainer

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.11.4
+
+WORKDIR /app
+
+RUN pip install julia jill ipython Flask --no-cache-dir
+# julia is pyjulia, our python-julia interface
+# jill is a python package for easy Julia installation
+# IPython is helpful for magic (both %time and %julia)
+# Include these in your requirements.txt if you have that instead
+
+RUN jill install 1.9.2 --confirm
+
+# PyJulia setup (installs PyCall & other necessities)
+RUN python -c "import julia; julia.install()"
+
+# Helpful Development Packages
+RUN julia -e 'using Pkg; Pkg.add(["BattMo", "Jutul"])'
--- app.py
+++ app.py
@@ -1,14 +1,85 @@
 from flask import Flask, render_template
 
-import sys
-import os
+import multiprocessing
+
+
+
 
 # sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # print(sys.path)
+#import julia
+
+#julia.install()
 
 #jl.seval('include("BattMoJulia/runP2DBattery.jl")')
+from julia import Main
+# import julia
+# julia.install()
+
+Main.include("BattMoJulia/runP2DBattery.jl")
 
 
+def run_julia():
+    print("start")
+    # runP2DBatt = Main.eval("""
+    # using BattMo, Jutul                       
+
+    # function runP2DBatt(json_file)
+
+
+    #     print("Calling BattMo simulation")
+    #     #states, reports, extra = runBattery_1d(input = jsondict, info_level = 0, extra_timing = false);
+    #     states, reports, extra = run_battery_1d(filename = json_file, info_level = -1, end_report = true, extra_timing = false);
+    #     print("Simulation finished")
+
+    #     con = BattMo.Constants()
+
+    #     # Get some result values
+    #     number_of_states = size(states)
+    #     timesteps = extra[:timesteps]
+    #     time_values = cumsum(timesteps)/con.hour
+    #     cell_voltage = [state[:BPP][:Phi][1] for state in states]
+    #     cell_current = [state[:BPP][:Current][1] for state in states]
+    #     negative_electrode_grid_wrap = physical_representation(extra[:model][:NAM])
+    #     electrolyte_grid_wrap = physical_representation(extra[:model][:ELYTE])
+    #     positive_electrode_grid_wrap = physical_representation(extra[:model][:PAM])
+    #     negative_electrode_concentration = Array([state[:NAM][:Cp] for state in states])/1000
+    #     electrolyte_concentration = [state[:ELYTE][:C] for state in states]/1000
+    #     positive_electrode_concentration = Array([state[:PAM][:Cp] for state in states])/1000
+    #     negative_electrode_potential = [state[:NAM][:Phi] for state in states]
+    #     electrolyte_potential = [state[:ELYTE][:Phi] for state in states]
+    #     positive_electrode_potential = [state[:PAM][:Phi] for state in states]
+
+    #     # Mesh cell centroids coordinates
+    #     centroids_NAM = negative_electrode_grid_wrap[:cell_centroids, Cells()]
+    #     centroids_ELYTE = electrolyte_grid_wrap[:cell_centroids, Cells()]
+    #     centroids_PAM = positive_electrode_grid_wrap[:cell_centroids, Cells()]
+
+    #     # Boundary faces coordinates
+    #     boundaries_NAM = negative_electrode_grid_wrap[:boundary_centroids, BoundaryFaces()]
+    #     boundaries_ELYTE = electrolyte_grid_wrap[:boundary_centroids, BoundaryFaces()]
+    #     boundaries_PAM = positive_electrode_grid_wrap[:boundary_centroids, BoundaryFaces()]
+
+    #     # Create grid arrays
+
+    #     negative_electrode_grid = [centroids_NAM, boundaries_NAM]
+    #     electrolyte_grid = [centroids_ELYTE, boundaries_ELYTE]
+    #     positive_electrode_grid = [centroids_PAM, boundaries_PAM]
+    #     #print([state[:BPP][:Phi] for state in states])
+    #     #print("time =", time_values.shape)
+    #     # print("pep =", positive_electrode_potential)
+    #     # print("nep =", negative_electrode_potential)  
+    #     #output = negative_electrode_concentration
+    #     output = [number_of_states, cell_voltage, cell_current, time_values, negative_electrode_grid, electrolyte_grid, positive_electrode_grid, negative_electrode_concentration, electrolyte_concentration, positive_electrode_concentration, negative_electrode_potential, electrolyte_potential, positive_electrode_potential];
+
+    #     return output
+    # end
+    #                     """)
+
+    
+    output = Main.runP2DBattery.runP2DBatt("BattMoJulia/p2d_40_jl.json")
+    #print(output)
+    return output
 
 
 app = Flask(__name__)
@@ -18,19 +89,29 @@ def json():
 
     return render_template('json.html')
 
-from julia import Main
 
-#jl.seval("using BattMo")
-Main.include("BattMoJulia/runP2DBattery.jl")
+
+
+
 
 #background process happening without any refreshing
 @app.route('/background_process_test')
 def background_process_test():
 
-
+    multiprocessing.set_start_method("spawn")
+    
+    #jl.seval("using BattMo")
+    #Main.include("BattMoJulia/runP2DBattery.jl")
     print ("Hello")
+    global process
+
+    
+    process = multiprocessing.Process(target = run_julia)
     #states, reports, extra = jl.seval('run_battery_1d(filename ="BattMoJulia/p2d_40_jl.json")')
-    states, reports, extra = Main.runP2DBatt("BattMoJulia/p2d_40_jl.json")
+    #run_julia()
+    
+    process.start()
+    process.join()
 
     print("simulation finished")
     return ("nothing")
@@ -39,4 +120,10 @@ def background_process_test():
 
     
     # return "<p>Voltage, = , %s </p>" % output(2)
+
+if __name__ == '__main__':
+    
+    #app.run(debug=True)
+    multiprocessing.set_start_method("spawn", force=True)
+    app.background_process_test(debug=True)
 
