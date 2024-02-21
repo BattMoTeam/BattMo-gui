431bb1ab63f3ecd6cd72603204c9887689c5b7a7
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-02-21 16:15:47 +0100

    added gevent module

--- streamlit/pages/1_Simulation.py
+++ streamlit/pages/1_Simulation.py
@@ -37,7 +37,16 @@ st.session_state.update(st.session_state)
 
 # Get page name
 url = str(st_javascript("await fetch('').then(r => window.parent.location.href)"))
-page_name = url.rsplit('/',1)[1]
+url_parts = url.rsplit('/',1)
+
+if len(url_parts) > 1:
+    # Extract the page name from the last part
+    page_name = url_parts[1]
+else:
+    # Handle the case where '/' is not found in the URL
+    page_name = "Unknown"
+
+
 
 
 def run_page():
--- streamlit/pages/2_Results.py
+++ streamlit/pages/2_Results.py
@@ -34,7 +34,14 @@ from app_scripts import app_view
 
 # Get page name
 url = str(st_javascript("await fetch('').then(r => window.parent.location.href)"))
-page_name = url.rsplit('/',1)[1]
+url_parts = url.rsplit('/',1)
+
+if len(url_parts) > 1:
+    # Extract the page name from the last part
+    page_name = url_parts[1]
+else:
+    # Handle the case where '/' is not found in the URL
+    page_name = "Unknown"
 
 
 def run_page():
