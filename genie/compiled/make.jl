cf0aba65834615d30b827486b5f1b30cd6e48a75
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-03-12 08:58:48 +0100

    sysimage not created

--- genie/compiled/make.jl
+++ genie/compiled/make.jl
@@ -1,22 +1,24 @@
+
+
 # make.jl
 using PackageCompiler
 
-include("packages.jl")
+#include("packages.jl")
 
 # Get the directory of the make.jl script
 script_dir = dirname(@__FILE__)
 
 # Specify the output path for the sysimg.so file relative to the script directory
-sysimage_path = joinpath(script_dir, "sysimg.so")
+sysimage_path = joinpath(script_dir,"sysimg.so")
+example_path = joinpath(script_dir, "compile_run_battery.jl")
+project_path = joinpath(dirname(script_dir))
 
 println("Sysimage path: ", sysimage_path)
 
-PackageCompiler.create_sysimage(
-  PACKAGES,
-  sysimage_path = "/app/compiled/sysimg.dylib",
-  precompile_execution_file="/app/compiled/compile_run_battery.jl",
-  cpu_target = PackageCompiler.default_app_cpu_target(),
-  sysimage_build_args = Cmd(["-O2"])
-)
+# Create the system image
+create_sysimage([:LoggingExtras,:Genie];
+	sysimage_path = sysimage_path,
+	project = project_path,
+	precompile_execution_file=example_path)
 
 println("End: Sysimg creation complete.")
