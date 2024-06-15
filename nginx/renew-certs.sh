5bac801335861cc75c8bea7ee49fbb9bf6423a6f
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-06-15 10:06:48 +0200

    added email adress to rener-certs.sh

--- nginx/renew-certs.sh
+++ nginx/renew-certs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 # Run Certbot to obtain or renew certificates
-certbot --nginx --non-interactive --agree-tos -d app.batterymodel.com
+certbot certonly --non-interactive --agree-tos --email lorena.hendrix@sintef.no --webroot -w /usr/share/nginx/html -d app.batterymodel.com
 
+# Verify if certificates have been obtained
+if [ -f /etc/letsencrypt/live/app.batterymodel.com/fullchain.pem ]; then
+  echo "Certificates have been obtained successfully."
+else
+  echo "Failed to obtain certificates."
+  exit 1
+fi
 # Reload Nginx configuration to apply new certificates
 nginx -s reload
