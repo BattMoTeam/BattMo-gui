666a99373291de3dded7ae69029036c0f9ac272f
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-06-15 13:12:54 +0200

    removed renew-certs debug line

--- nginx/renew-certs.sh
+++ nginx/renew-certs.sh
@@ -3,12 +3,6 @@
 # Run Certbot to obtain or renew certificates
 certbot certonly --non-interactive --agree-tos --email lorena.hendrix@sintef.no --webroot -w /usr/share/nginx/html -d app.batterymodel.com
 
-# Verify if certificates have been obtained
-if [ -f /etc/letsencrypt/live/app.batterymodel.com/fullchain.pem ]; then
-  echo "Certificates have been obtained successfully."
-else
-  echo "Failed to obtain certificates."
-  exit 1
-fi
+
 # Reload Nginx configuration to apply new certificates
 nginx -s reload
