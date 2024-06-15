2bf278dd1a44d7581758dff92f0cf644d505201b
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-06-15 20:14:19 +0200

    added an entrypoint to nginx docker file

--- nginx/renew-certs.sh
+++ nginx/renew-certs.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-# Stop existing containers
-docker compose down
+set -e
 
 # Obtain/renew SSL certificates
 certbot certonly --webroot -w /usr/share/nginx/html -d app.batterymodel.com --agree-tos --email lorena.hendrix@sintef.no
-
-# Start containers again
-docker compose up -d
-
 # Reload Nginx to apply new certificates
 nginx -s reload
