0160dc465d613e17bdc91fc9742dc4b3f18275b7
Lorena Hendrix <lorenahendrix163@gmail.com>
2024-06-16 12:14:23 +0200

    added volume to nginx in docker-compose file to copy ssl certificates from host to container

--- nginx/renew-certs.sh
+++ nginx/renew-certs.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 set -e
 
-# Function to start Nginx in the background
-start_nginx() {
-    nginx -g 'daemon off;' &
-    NGINX_PID=$!
-}
+certbot certonly --webroot -w /usr/share/nginx/html -d app.batterymodel.com --email lorena.hendrix@sintef.no --agree-tos --non-interactive
 
-# Function to stop the Nginx process
-stop_nginx() {
-    if [ -n "$NGINX_PID" ]; then
-        kill "$NGINX_PID"
-    fi
-}
 
-# Trap to ensure Nginx is stopped on script exit
-trap stop_nginx EXIT
-
-# Start Nginx to serve the challenge files
-start_nginx
-
-# Obtain or renew certificates
-if [ ! -d "/etc/letsencrypt/live/app.batterymodel.com" ]; then
-    certbot certonly --webroot -w /usr/share/nginx/html -d app.batterymodel.com --email lorena.hendrix@sintef.no --agree-tos --non-interactive
-else
-    certbot renew --webroot -w /usr/share/nginx/html
-fi
-
-# Stop the background Nginx process
-stop_nginx
-
-# Start Nginx in the foreground
-exec nginx -g 'daemon off;'
